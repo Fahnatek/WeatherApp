6422d5d8d4c4b166c9fb897ef5ba80990ea32e51
Joe <jhnatek07@gmail.com>
2017-12-26 17:13:55 -0600

    v1.2.0
    +Wind chill advisory
    +Wind speed

--- WeatherApp.py
+++ WeatherApp.py
@@ -19,13 +19,14 @@ def geolocate():
 
     return(zip)
 
+
 def getWeatherData(zipCode):
     import urllib.request as link
     import json
     import apikey
 
     API_KEY = apikey.getKey() #Get your own key at: http://openweathermap.org/appid
-    
+
     url = ("http://api.openweathermap.org/data/2.5/weather?zip=" + zipCode + "&appid=" + API_KEY)
 
     data = link.urlopen(url)
@@ -36,23 +37,37 @@ def getWeatherData(zipCode):
 
     return(data)
 
+
 def getWeather(Data):
-    strData = str(Data)
-
     location = Data["name"]
-    kelvin = Data["main"]["temp"]
 
-    fahrenheit = int(1.8 *(kelvin - 273) + 32)
+    kelvin = Data["main"]["temp"]               #   Temperature
+    fahrenheit = int(1.8 *(kelvin - 273) + 32)  #   Converting Kelvin to Fahrenheit
+
+    """TODO calculate where the wind is blowing to"""
+    wsMPS = Data['wind']['speed']   #   Wind speed in meters per second
+    wsMPH = int(wsMPS * 2.2369)        #   Converting Wind speed m/s to mph
+
+    if (fahrenheit <= 50 and wsMPH >= 3.0):  #  Wind chill in effect
+        windchill = int(35.74 + (0.6215*fahrenheit) - 35.75*(wsMPH**0.16) + (0.4275*fahrenheit)*(wsMPH**0.16))
+    else:
+        windchill = "None"
+
+
+
 
     main = Data["weather"][0]["main"]
     mainDes = Data["weather"][0]["description"]
-    
-    return (location, fahrenheit, main, mainDes)
-    
-def printWeather(location, temp, curMain, mainDes):
+
+    return location, fahrenheit, main, mainDes, wsMPH, windchill
+
+
+def printWeather(location, temp, curMain, mainDes, windSpeed, windChill):
     print("")
-    print(location + ": " , temp , "°F")
+    print(location + ": ", temp , "°F | Wind Chill Advisory: ", windChill, "°F")
     print(curMain + ": " + mainDes)
+    print("Wind Speed: ", windSpeed, "mph")
+    
 
 
 def main():
@@ -62,17 +77,18 @@ def main():
     while True:
         currentTime = datetime.datetime.today()
         print("Gathering weather information for your location... at:", currentTime.strftime('%I:%M%p'))
-       
+
         zip = geolocate()
         data = getWeatherData(zip)
-        location, temp, curMain, mainDes = getWeather(data)
-        printWeather(location, temp, curMain, mainDes)
+        location, temp, curMain, mainDes, windSpeed, windChill = getWeather(data)
+        printWeather(location, temp, curMain, mainDes, windSpeed, windChill)
 
         time.sleep(1200)
         print("\n")
 
+
 if __name__ == "__main__":
-    version = "1.1.2"
+    version = "1.2.0"
     print("Version Number: ", version)
     main()
 
